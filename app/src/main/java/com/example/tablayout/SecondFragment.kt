489f939eedc29bf7fe8eb8ca5bce511b05ae49a1
SekarAisyah <sekarrisma@apps.ipb.ac.id>
2023-03-27 16:54:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tablayout/SecondFragment.kt
@@ -0,0 +1,32 @@
+package com.example.tablayout
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tablayout.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSecondBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupData()
+    }
+
+    private fun setupData() {
+        binding.label.text = getString(R.string.second_fragment)
+    }
+}
---
+++ app/src/main/java/com/example/tablayout/ThirdFragment.kt
@@ -0,0 +1,32 @@
+package com.example.tablayout
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tablayout.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+
+    private lateinit var binding: FragmentThirdBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentThirdBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupData()
+    }
+
+    private fun setupData() {
+        binding.label.text = getString(R.string.third_fragment)
+    }
+}
